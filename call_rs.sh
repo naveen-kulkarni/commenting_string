2949a64b4fda1f9918b51e96b4584271bb8cd5a4
naveen-kulkarni <33716521+naveen-kulkarni@users.noreply.github.com>
2023-04-16 22:11:38 +0530

    Update call_rs.sh

--- call_rs.sh
+++ call_rs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sh /home/nkulka99/AutoNaveen/solarisSed/remove_solaris.sh >>  /home/nkulka99/AutoNaveen/solarisSed/log-010620 2>&1
+sh /home/naveen/AutoNaveen/solarisSed/remove_solaris.sh >>  /home/naveen/AutoNaveen/solarisSed/log-010620 2>&1
